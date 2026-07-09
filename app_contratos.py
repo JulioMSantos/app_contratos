1600383fba614fcbee69f59097e76aec27f141e6
JulioMSantos <julinhosantos80@gmail.com>
2026-07-09 09:35:28 -0300

    Refactor app_contratos.py for improved clarity

--- app_contratos.py
+++ app_contratos.py
@@ -173,7 +173,7 @@ def gerar_fluxograma(etapa_destaque=None):
                 # Regras de Destaque
                 if etapa_destaque and id_caixa == etapa_destaque:
                     # Projeto atual = Amarelo Destaque
-                    c.node(id_caixa, texto_real, shape=formato, style='filled', fillcolor='#FFD700', penwidth='3', fontname='Helvetica-Bold',fontsize='14')
+                    c.node(id_caixa, texto_real, shape=formato, style='filled', fillcolor='#FFD700', penwidth='3', fontname='Helvetica-Bold', fontsize='18')
                 else:
                     # Resto = Branco padrão
                     c.node(id_caixa, texto_real, shape=formato, style='filled', fillcolor='white', fontname='Helvetica', fontsize='10')
